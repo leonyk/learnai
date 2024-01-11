fea58ec03ff11fa849e8f49eb5a2d6c893a1dcad
Kai Yin <leonyk@gmail.com>
2024-01-11 08:19:09 +0000

    initial commit

---
+++ test_langchain.py
@@ -0,0 +1,27 @@
+
+# from langchain_community.chat_models import ChatOpenAI
+# from langchain_community.document_loaders import TextLoader
+# from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.document_loaders import TextLoader
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+
+embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
+loader = TextLoader("state_of_the_union.txt")
+index = VectorstoreIndexCreator(embedding=embedding).from_loaders([loader])
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+questions = [
+    "Who is the speaker",
+    "What did the president say about Ketanji Brown Jackson",
+    "What are the threats to America",
+    "Who are mentioned in the speech",
+    "Who is the vice president",
+    "How many projects were announced",
+]
+
+for query in questions:
+    print("Query:", query)
+    print("Answer:", index.query(query, llm=llm))
+    
